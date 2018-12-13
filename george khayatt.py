65196356a1358c54e52d7a21c7fa29e7277fe96a
gkhayatt23 <45853372+gkhayatt23@users.noreply.github.com>
2018-12-13 10:16:18 -0500

    Add files via upload

---
+++ george khayatt.py
@@ -0,0 +1,120 @@
+import pyautogui as pg
+import webbrowser as wb
+
+
+show= pg.prompt ("what is your favorite show?")
+if show == "billions":
+    wb.open("https://www.youtube.com/watch?v=mGIpOtncmSM")
+    pg.alert ("good choice")
+elif show== "the office":
+    wb.open("https://www.youtube.com/watch?v=tTT57_YK6Q4")
+    pg.alert("NICE.io")
+elif show== "Friends":
+    wb.open("https://www.youtube.com/watch?v=LDU_Txk06tM")
+    pg.alert ("chanler bing is me hero")
+elif show== "suits":
+    
+    pg.alert ("I am harvey specter")
+elif show== "vitourias":
+    pg.alert ("that is a dank one")
+elif show== "NFL":
+    pg.alert ("what the WWE")
+else:
+    pg.alert ("Your favorite show is "+ show)
+
+Pokemon= pg.prompt ("what is your favorite pokemon?")
+
+if Pokemon == "pikachu":
+    wb.open("https://www.youtube.com/watch?v=zz2ZOqmO0KU")
+    pg.alert ("good choice")
+elif Pokemon== "charzard":
+    wb.open("https://www.youtube.com/watch?v=_GaY_Qsf6o8")
+    pg.alert("He is OP")
+elif Pokemon== "pikachu":
+    wb.open("https://www.youtube.com/watch?v=wdNLzWSoLWg")
+    pg.alert("He is a meme")
+elif Pokemon== "snorlax":
+    wb.open("https://www.youtube.com/watch?v=c8CHDTpbh5w")
+    pg.alert("G.O.A.T.")
+elif Pokemon== "ditto":
+    pg.alert("He is my spirit animal")
+elif Pokemon== "Eevee":
+    pg.alert("Cute")
+else:
+    pg.alert ("U DUMB")
+
+fort= pg.prompt ("what is your favorite fortnite Clan?")
+
+if fort == "Stud clan":
+    wb.open("https://www.youtube.com/watch?v=5nfv_iEeC8Y")
+    pg.alert ("@stud_Crow")
+elif fort== "Faze Clan":
+    wb.open("")
+    pg.alert("@Not_Tfue")
+elif fort== "Icy Clan":
+    wb.open("")
+    pg.alert("Best Fortnite clan of all time!")
+elif fort== "TSM":
+    wb.open("")
+    pg.alert("@TSM_Deaquan")
+elif fort== "Liquid":
+    pg.alert("@Liquid_Poach")
+elif fort== "C9":
+    pg.alert("Coolest Name")
+else:
+    pg.alert ("U DUMB")
+    
+food= pg.prompt ("what is your favorite food?")
+
+if food == "Sushi":
+    wb.open("https://www.youtube.com/watch?v=M2gYRwdcI6o")
+    pg.alert ("Yummy in my Tummy")
+elif food== "Pizza":
+    wb.open("")
+    pg.alert("Kool")
+elif food== "Dumplings":
+    wb.open("")
+    pg.alert("Clay Griffin broke the Dumpling previously set by Gordo")
+elif food== "Cheetos":
+    wb.open("")
+    pg.alert("Peter Kaplan got kicked out of band for putting Cheetos in a Trumpet. ")
+elif food== "Liquid":
+    pg.alert("Tasty")
+elif food== "":
+    pg.alert("Coolest Name")
+else:
+    pg.alert ("U DUMB")
+
+Football= pg.prompt ("what is your favorite football team?")
+
+if Football == "Green Bay Packers":
+    wb.open("https://www.youtube.com/watch?v=fvtQYsckLxk")
+    pg.alert ("Nice choice!!!!!!!!!!!!!!!!!!!!!!!!!!!!")
+elif Football== "Patriots":
+    wb.open("")
+    pg.alert("Kool...........................................................")
+elif Football== "Steelers":
+    wb.open("")
+    pg.alert("o")
+elif Football== "Redskins":
+    wb.open("")
+    pg.alert("Peter Kaplan.")
+elif Football== "Cardinals":
+    pg.alert("BAD")
+elif Football== "Seahawks":
+    pg.alert("Coolest jersys")
+else:
+    pg.alert ("They Trash")
+
+
+
+
+
+
+
+
+
+
+
+
+    
